0cab1f309f3b5b8a64aa1238cb27cef8619874bc
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-01-23 21:36:27 -0300

    feature: creating models and adding config in settings.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField('Name', max_length=75)
+
+
+class Datapoint(models.Model):
+    plant_id = models.ForeignKey(Plant, on_delete=models.DO_NOTHING)
+    energy_expected = models.FloatField('Expected energy')
+    energy_observed = models.FloatField('Observed energy')
+    irradiation_observed = models.FloatField('Observed irradiation')
+    irradiation_expected = models.FloatField('Expected irradiation')
