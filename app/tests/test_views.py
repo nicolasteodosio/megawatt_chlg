2f83d7cd18755d31206cd7475a2067d7d898bb28
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-01-25 11:27:24 -0300

    feature: testing plan detail

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import resolve_url
 from django.test import TestCase
 from model_mommy import mommy
@@ -19,3 +21,58 @@ class TestPlantList(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content.decode('utf-8'), '[]')
+
+
+class TestPlantDetail(TestCase):
+
+    def test_get_plant(self):
+        mommy.make(Plant, id=555)
+        response = self.client.get(resolve_url('plant_detail', 555))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.content)
+
+    def test_get_plant_dont_exist(self):
+        mommy.make(Plant, id=555)
+        response = self.client.get(resolve_url('plant_detail', 78))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_plant_detail_method_not_allowed(self):
+        mommy.make(Plant, id=555)
+        response = self.client.post(resolve_url('plant_detail', 555))
+
+        self.assertEqual(response.status_code, 405)
+
+    def test_update_plant(self):
+        mommy.make(Plant, id=44, name="plant1")
+        payload = {
+            'name': 'plant2'
+        }
+        self.assertEqual(Plant.objects.get(id=44).name, 'plant1')
+        response = self.client.put(resolve_url('plant_detail', 44), json.dumps(payload),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Plant.objects.get(id=44).name, 'plant2')
+
+    def test_update_plant_if_exists(self):
+        mommy.make(Plant, id=44, name="plant1")
+        payload = {
+            'nome': 'plant2'
+        }
+        response = self.client.put(resolve_url('plant_detail', 99), json.dumps(payload), content_type='application/json')
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_plant(self):
+        plant = mommy.make(Plant)
+        response = self.client.delete(resolve_url('plant_detail', plant.id))
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(list(Plant.objects.filter(id=plant.id)), [])
+
+    def test_delete_plant_if_exists(self):
+        response = self.client.delete(resolve_url('plant_detail', 456))
+
+        self.assertEqual(response.status_code, 404)
