f6c3c166a56ea8142e59d4c32cec3612ac5d702e
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-01-25 10:57:29 -0300

    feature: creating views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from app.models import Plant
+from app.serializers import PlantSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class PlantList(generics.ListCreateAPIView):
+    queryset = Plant.objects.all()
+    serializer_class = PlantSerializer
+
+
+class PlantDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Plant.objects.all()
+    serializer_class = PlantSerializer
