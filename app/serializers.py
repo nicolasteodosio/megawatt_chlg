b7383c7d53ca968277984f1a8e5174a223b2f888
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-01-26 22:50:26 -0300

    feature: serializer for report

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from app.models import Plant
+from app.models import Plant, Report
 
 
 class PlantSerializer(serializers.ModelSerializer):
@@ -9,8 +9,12 @@ class PlantSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
-class DatapointSerializer(serializers.ModelSerializer):
+class ReportSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Plant
-        fields = ['id', 'plant_id', 'energy_expected', 'energy_observed',
-                  'irradiation_observed', 'irradiation_expected']
+        model = Report
+        fields = ['plant',
+                  'type',
+                  'expected',
+                  'observed',
+                  'date_created',
+                  'monitoring_date']
