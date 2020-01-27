ed862815431607dd8d3a3cf52fd0bb3ceac2e6da
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-01-26 22:42:29 -0300

    feature: improving and adding dates

--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 from djchoices import DjangoChoices, ChoiceItem
 
 
 class Plant(models.Model):
     name = models.CharField('Name', max_length=75)
+
+    def __str__(self):
+        return self.name
 
 
 class Report(models.Model):
@@ -12,7 +17,12 @@ class Report(models.Model):
         energy = ChoiceItem('Energy')
         irradiation = ChoiceItem('Irradiation')
 
-    plant_id = models.ForeignKey(Plant, on_delete=models.DO_NOTHING)
+    plant = models.ForeignKey(Plant, on_delete=models.DO_NOTHING)
     type = models.CharField('Type of report', choices=TypesChoices.choices, max_length=15)
     expected = models.FloatField('Expected')
     observed = models.FloatField('Observed')
+    date_created = models.DateTimeField('Created date', auto_now_add=True)
+    monitoring_date = models.DateTimeField('Monitoring date', default=datetime.now())
+
+    def __str__(self):
+        return '{} - {}'.format(self.plant_id, self.type)
