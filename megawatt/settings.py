014964e48438f858c10cd826c813626bc106bc9a
Nicolas Teodosio <nicolas.teodosio@luizalabs.com>
2020-01-29 01:17:28 -0300

    feature: adding prod database config

--- megawatt/settings.py
+++ megawatt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import dj_database_url
 from celery.schedules import crontab
 from dotenv import load_dotenv
 load_dotenv()
@@ -80,10 +81,9 @@ WSGI_APPLICATION = 'megawatt.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
